19738c4c4869fa46f811815f251f30332af969fc
jjsly112233 <137158422+jjsly112233@users.noreply.github.com>
2024-04-08 12:55:18 +0000

    update

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -9,6 +9,10 @@
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor(i32, i32, i32),
+    Echo(String),
+    Move {x:i32,y:i32},
+    Quit,
 }
 
 struct Point {
